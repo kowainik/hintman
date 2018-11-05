5b2b43920f299371ce0a79f6e84eecdebf704ee7
Giovanni Ornaghi <sphaso@gmail.com>
2018-11-05 16:55:22 +0100

    [#28] Verify request origin (#30)
    * [#28] Verify request origin
    
    * print and style
    
    * moved github digest in where

--- src/Hintman/Server/Auth.hs
+++ src/Hintman/Server/Auth.hs
@@ -8,18 +8,48 @@ module Hintman.Server.Auth
        , hintmanAuthServerContext
        ) where
 
-import Network.Wai (Request)
-import Servant (Context ((:.), EmptyContext))
+import Crypto.Hash (Digest, digestFromByteString)
+import Crypto.Hash.Algorithms (SHA1)
+import Crypto.MAC.HMAC (HMAC (..), hmac)
+import Data.ByteString (breakSubstring)
+import Data.List (lookup)
+import Network.Wai (Request (..))
+import Servant (Context ((:.), EmptyContext), throwError)
 import Servant.API (AuthProtect)
-import Servant.Server (Handler)
+import Servant.Server (Handler, ServantErr (..), err400, err401)
 import Servant.Server.Experimental.Auth (AuthHandler, AuthServerData, mkAuthHandler)
+import System.Environment (getEnv)
 
+import Hintman.GitHub.Auth (GithubWebhookSecret (..))
 
 type HintmanAuthAPI = AuthProtect "GitHub"
 type instance AuthServerData (AuthProtect "GitHub") = ()
 
 type HintmanAuthContextHandlers = '[AuthHandler Request ()]
 type HintmanAuthContext = Context HintmanAuthContextHandlers
+
+verifySignature :: Request -> Handler ()
+verifySignature req = do
+    body <- liftIO $ requestBody req
+    verifySignature' body header
+  where
+    header :: Maybe ByteString
+    header = lookup "x-hub-signature" (requestHeaders req)
+
+verifySignature' :: ByteString -> Maybe ByteString -> Handler ()
+verifySignature' _ Nothing = throwError $ err401 { errBody = "Empty signature." }
+verifySignature' payload (Just signature) = do
+    GithubWebhookSecret key <- liftIO $  GithubWebhookSecret . toText <$> getEnv "GITHUB_WEBHOOK_SECRET"
+    let ourDigest = sha1sum (encodeUtf8 key) payload
+    case theirDigest of
+      Nothing -> throwError $ err400 { errBody = "Invalid signature." }
+      Just d  -> unless (d == ourDigest) $ throwError $ err401 { errBody = "Wrong signature."}
+  where
+    theirDigest :: Maybe (Digest SHA1)
+    theirDigest = digestFromByteString $ snd $ breakSubstring signature "="
+
+    sha1sum :: ByteString -> ByteString -> Digest SHA1
+    sha1sum key msg = hmacGetDigest $ hmac key msg
 
 hintmanAuthServerContext :: HintmanAuthContext
 hintmanAuthServerContext = hintmanAuthHandler :. EmptyContext
@@ -28,4 +58,4 @@ hintmanAuthHandler :: AuthHandler Request ()
 hintmanAuthHandler = mkAuthHandler handler
   where
     handler :: Request -> Handler ()
-    handler = liftIO . print
+    handler = verifySignature >> print
