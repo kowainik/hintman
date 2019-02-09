5cd1552b23e376d2a69327860cfbebe872c66265
Dmitrii Kovanikov <kovanikov@gmail.com>
2019-02-09 19:06:31 +0800

    [#37] Comment signature verification (#42)
    Resolves #37

--- src/Hintman/Server/Auth.hs
+++ src/Hintman/Server/Auth.hs
@@ -9,19 +9,11 @@ module Hintman.Server.Auth
        , hintmanAuthServerContext
        ) where
 
-import Crypto.Hash (Digest, digestFromByteString)
-import Crypto.Hash.Algorithms (SHA1)
-import Crypto.MAC.HMAC (HMAC (..), hmac)
-import Data.ByteString (breakSubstring)
-import Data.List (lookup)
-import Network.Wai (Request (..))
-import Servant (Context ((:.), EmptyContext), throwError)
+import Network.Wai (Request)
+import Servant (Context ((:.), EmptyContext))
 import Servant.API (AuthProtect)
-import Servant.Server (Handler, ServantErr (..), err400, err401)
+import Servant.Server (Handler)
 import Servant.Server.Experimental.Auth (AuthHandler, AuthServerData, mkAuthHandler)
-import System.Environment (getEnv)
-
-import Hintman.GitHub.Auth (GithubWebhookSecret (..))
 
 
 type HintmanAuthAPI = AuthProtect "GitHub"
@@ -30,28 +22,28 @@ type instance AuthServerData (AuthProtect "GitHub") = ()
 type HintmanAuthContextHandlers = '[AuthHandler Request ()]
 type HintmanAuthContext = Context HintmanAuthContextHandlers
 
-verifySignature :: Request -> Handler ()
-verifySignature req = do
-    body <- liftIO $ requestBody req
-    verifySignature' body header
-  where
-    header :: Maybe ByteString
-    header = lookup "x-hub-signature" (requestHeaders req)
-
-verifySignature' :: ByteString -> Maybe ByteString -> Handler ()
-verifySignature' _ Nothing = throwError $ err401 { errBody = "Empty signature." }
-verifySignature' payload (Just signature) = do
-    GithubWebhookSecret key <- liftIO $  GithubWebhookSecret . toText <$> getEnv "GITHUB_WEBHOOK_SECRET"
-    let ourDigest = sha1sum (encodeUtf8 key) payload
-    case theirDigest of
-      Nothing -> throwError $ err400 { errBody = "Invalid signature." }
-      Just d  -> unless (d == ourDigest) $ throwError $ err401 { errBody = "Wrong signature."}
-  where
-    theirDigest :: Maybe (Digest SHA1)
-    theirDigest = digestFromByteString $ snd $ breakSubstring signature "="
-
-    sha1sum :: ByteString -> ByteString -> Digest SHA1
-    sha1sum key msg = hmacGetDigest $ hmac key msg
+-- verifySignature :: Request -> Handler ()
+-- verifySignature req = do
+--     body <- liftIO $ requestBody req
+--     verifySignature' body header
+--   where
+--     header :: Maybe ByteString
+--     header = lookup "x-hub-signature" (requestHeaders req)
+--
+-- verifySignature' :: ByteString -> Maybe ByteString -> Handler ()
+-- verifySignature' _ Nothing = throwError $ err401 { errBody = "Empty signature." }
+-- verifySignature' payload (Just signature) = do
+--     GithubWebhookSecret key <- liftIO $  GithubWebhookSecret . toText <$> getEnv "GITHUB_WEBHOOK_SECRET"
+--     let ourDigest = sha1sum (encodeUtf8 key) payload
+--     case theirDigest of
+--       Nothing -> throwError $ err400 { errBody = "Invalid signature." }
+--       Just d  -> unless (d == ourDigest) $ throwError $ err401 { errBody = "Wrong signature."}
+--   where
+--     theirDigest :: Maybe (Digest SHA1)
+--     theirDigest = digestFromByteString $ snd $ breakSubstring signature "="
+--
+--     sha1sum :: ByteString -> ByteString -> Digest SHA1
+--     sha1sum key msg = hmacGetDigest $ hmac key msg
 
 hintmanAuthServerContext :: HintmanAuthContext
 hintmanAuthServerContext = hintmanAuthHandler :. EmptyContext
@@ -60,6 +52,4 @@ hintmanAuthHandler :: AuthHandler Request ()
 hintmanAuthHandler = mkAuthHandler handler
   where
     handler :: Request -> Handler ()
-    handler req = do
-        verifySignature req
-        print req
+    handler = print
